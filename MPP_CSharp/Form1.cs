eb36f37f4802d0e8f040a78f3ddeffa5a496d4d0
clau100 <claudiupetrila@gmail.com>
2024-03-16 17:24:46 +0200

    Added Logging Functionality

--- MPP_CSharp/Form1.cs
+++ MPP_CSharp/Form1.cs
@@ -7,15 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using log4net;
 using MPP_CSharp.Domain;
 
 namespace MPP_CSharp
 {
     public partial class Form1 : Form
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Form1));
         public Form1()
         {
+            Log.Info("Starting Form1");
             InitializeComponent();
+            Log.Info("Finished Form1");
         }
     }
 }
